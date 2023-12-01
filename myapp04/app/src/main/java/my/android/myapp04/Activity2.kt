bfc502e42a9920b167b0cc8c50438c9e130c21cc
yand <yand@outlook.com>
2023-12-01 11:48:25 +0300

    YA

--- myapp04/app/src/main/java/my/android/myapp04/Activity2.kt
+++ myapp04/app/src/main/java/my/android/myapp04/Activity2.kt
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class Activity1 : AppCompatActivity() {
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_12)
         val txt1 = findViewById<TextView>(R.id.myNumber)
         txt1.text = MyData.counter.toString();
         MyData.counter += 1
+        val root = findViewById<ConstraintLayout>(R.id.root)
+        root.setBackgroundColor(0x55FFFF00)
 
         val next = findViewById<Button>(R.id.next)
         next.setOnClickListener {
-            val intent = Intent(this, Activity2::class.java)
+            val intent = Intent(this, Activity1::class.java)
             startActivity(intent)
         }
     }
